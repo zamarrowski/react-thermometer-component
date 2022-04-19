d06f0b65ad76ea669513c0e488573e73436874fb
bigbizze <42508203+bigbizze@users.noreply.github.com>
2022-04-19 12:06:11 -0400

    Update index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,10 +1,10 @@
-  interface ThermometerProps {
-    theme?: "light" | "dark",
-    value?: number,
-    max?: number,
-    steps?: number,
-    format?: `°C` | `°F`,
-    size?: "large" | "small" | "normal",
-    height?: number
-  }
-  export default function Thermometer(props: ThermometerProps): React.ReactNode;
+interface ThermometerProps {
+  theme?: "light" | "dark",
+  value?: number,
+  max?: number,
+  steps?: number,
+  format?: string,
+  size?: "large" | "small" | "normal",
+  height?: number
+}
+export default function Thermometer(props: ThermometerProps): JSX.Element;
